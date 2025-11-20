59dd5f9f7cd56b21ccc2f8855f58d2b6e1d73992
Laura Delissen <lauradelissen@hotmail.com>
2025-11-20 18:17:45 +0100

    GameLoop gemaakt

--- XRpedition/Assets/Scripts/Fish Game/FishBase.cs
+++ XRpedition/Assets/Scripts/Fish Game/FishBase.cs
@@ -5,6 +5,13 @@ using Unity.VisualScripting;
 
 public abstract class FishBase : MonoBehaviour
 {
+    private GameLoop gameLoop;
+    [SerializeField] string CurrentColor; 
+
+    void Start()
+    {
+        gameLoop = GameObject.FindWithTag("GameLoop").GetComponent<GameLoop>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,8 +24,17 @@ public abstract class FishBase : MonoBehaviour
     public void Caught()
     {
         //code for being caught
+        if (gameLoop.CurrentColor == CurrentColor)
+        {
+            gameLoop.RightFish();
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameLoop.WrongFish();
+            Destroy(gameObject);
+        }
         
-        Destroy(gameObject);
     }
     
     
--- XRpedition/Assets/Scripts/Fish Game/GameLoop.cs
+++ XRpedition/Assets/Scripts/Fish Game/GameLoop.cs
@@ -1,15 +1,108 @@
-﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class FishSpawner : MonoBehaviour
+public class GameLoop : MonoBehaviour
 {
-    [SerializeField] List<GameObject> FishPrefabs;
-    public GameObject chosenFish { private set; get; }
-    
-    
-    
-    void ChooseNewFish()
+    [SerializeField] private GameObject Canvas;
+    [SerializeField] private float TimeActive = 2f;
+
+    private GameObject CatchRed;
+    private GameObject CatchBlue;
+    private GameObject CatchGreen;
+    private GameObject CatchYellow;
+    private GameObject CatchPurple;
+    private GameObject Wrong;
+    private GameObject Right;
+
+    private GameObject[] allCatchUI;
+    private GameObject currentActiveUI;
+
+    [SerializeField] public string CurrentColor; /*{ get; private set; }*/
+
+
+    void Awake()
     {
+        CatchRed = Canvas.transform.Find("Red").gameObject;
+        CatchBlue = Canvas.transform.Find("Blue").gameObject;
+        CatchGreen = Canvas.transform.Find("Green").gameObject;
+        CatchYellow = Canvas.transform.Find("Yellow").gameObject;
+        CatchPurple = Canvas.transform.Find("Purple").gameObject;
+        Wrong = Canvas.transform.Find("Wrong").gameObject;
+        Right = Canvas.transform.Find("Right").gameObject;
+
+        allCatchUI = new GameObject[] {
+            CatchRed, CatchBlue, CatchGreen, CatchYellow, CatchPurple
+        };
+    }
+
+    void Start()
+    {
+        DisableAllUI();
+        EnableUI("fish");
+    }
+
+    private void EnableUI(string x)
+    {
+        // Kies random UI object
+        if (x == "fish")
+        {
+            int randomIndex = Random.Range(0, allCatchUI.Length);
+            currentActiveUI = allCatchUI[randomIndex];
+
+            currentActiveUI.SetActive(true);
+            CurrentColor = currentActiveUI.name;
+        }
+        else if (x == "right")
+        {
+            currentActiveUI = Right;
+            currentActiveUI.SetActive(true);
+            EnableUI("fish");
+        }        
+        else if (x == "wrong")
+        {
+            currentActiveUI = Wrong;
+            currentActiveUI.SetActive(true);
+            EnableUI("fish");
+        }
         
+
+        // Na TimeActive weer uit
+        StartCoroutine(DisableAfterTime());
+    }
+
+    private IEnumerator DisableAfterTime()
+    {
+        yield return new WaitForSeconds(TimeActive);
+        DisableUI();
+    }
+
+    private void DisableUI()
+    {
+        if (currentActiveUI != null)
+            currentActiveUI.SetActive(false);
+    }
+
+    private void DisableAllUI()
+    {
+        foreach (var ui in allCatchUI)
+            ui.SetActive(false);
+        
+        Wrong.SetActive((false));
+        Right.SetActive((false));
+    }
+    
+
+    public void WrongFish()
+    {
+        Debug.Log("Wrong fish!");
+        // eventueel nieuwe UI starten
+        EnableUI("wrong");
+    }
+
+    public void RightFish()
+    {
+        Debug.Log("Right fish!");
+        // eventueel nieuwe UI starten
+        EnableUI("right");
     }
 }
